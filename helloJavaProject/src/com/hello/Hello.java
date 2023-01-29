6a5ecaf04db4680de3475124503616fe7b36c87f
fortrainees2020 <64248056+fortrainees2020@users.noreply.github.com>
2023-01-29 16:03:37 +0530

    Add files via upload

---
+++ helloJavaProject/src/com/hello/Hello.java
@@ -0,0 +1,10 @@
+package com.hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	System.out.println(" Hello Jenkin from Git");	// TODO Auto-generated method stub
+
+	}
+
+}
